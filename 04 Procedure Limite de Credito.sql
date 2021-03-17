4af7cde1c1879c43b47d7a857865eb6a70073d12
Cinthia Barbosa da Silva <66570013+cinthiabs@users.noreply.github.com>
2021-03-17 11:35:46 -0300

    Estudos

---
+++ 04 Procedure Limite de Credito.sql
@@ -0,0 +1,51 @@
+
+create procedure LimitCred   
+  
+  
+@cliente int ,  
+@credito smallmoney   
+  
+AS  
+begin   
+  
+ if exists(select iIDCliente from tCADCliente where iIDCliente=@cliente and nTipoPessoa=1 )  
+  begin  
+   if @credito <= 400.00  
+   begin  
+    update tCADCliente   
+    set mCredito=@credito  
+    where iIDCliente=@cliente  
+      
+    select 'Limite atualizado ' +CAST(@credito as varchar(30))+ ''  
+   end  
+  else   
+  begin   
+   select 'Limite permitido para pessoa fisica R$400,00'  
+  end  
+     
+ end  
+ else if  exists(select iIDCliente from tCADCliente where iIDCliente=@cliente and nTipoPessoa=2 )  
+  begin   
+   if @credito <=900.00  
+   begin   
+   update tCADCliente   
+   set mCredito = @credito  
+   where iIDCliente=@credito  
+     
+   select 'Limite atualizado ' +CAST(@credito as varchar(30))+ ''  
+  end  
+  else   
+  begin   
+   select 'Limite permitido para pessoa Juridica R$900,00'  
+  end  
+  
+ end  
+ else if @cliente = null or @credito = null  
+  begin   
+   select 'Necessário inserir todas as informações.'  
+  end  
+  
+ else  
+ select 'Dados não encontrado.'  
+  
+end 
